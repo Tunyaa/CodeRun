ded5380d01250548fb8861872d8dea448270eeae
Tunyaa <oldcampthunder@gmail.com>
2023-08-17 22:39:38 +0300

    Nails

---
+++ CodeRun/src/main/java/com/java/coderun/tasks/Nails.java
@@ -0,0 +1,24 @@
+
+package com.java.coderun.tasks;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+// Задача https://coderun.yandex.ru/problem/pin/description?currentPage=1&pageSize=10&rowNumber=1&compiler=java
+
+public class Nails {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        
+
+        reader.close();
+        writer.close();
+    }
+}
